ac71cfabfaa95cb33bb60e389f93b165ca7c755c
huangdailin <huangdailin@thunisoft.com>
2019-07-15 17:54:42 +0800

    【+】新建

---
+++ Lambda/src/com/thunisoft/lambda/Program.java
@@ -0,0 +1,15 @@
+package com.thunisoft.lambda;
+
+/**
+ * java类简单作用描述
+ *
+ * @Author: huangdailin
+ * @CreateDate: 2019/7/15 17:25
+ * @Version: 1.0
+ */
+public class Program {
+}
+
+interface Comparator {
+
+}
